a9515eb72eb21910cd61fd09c66302647d90b311
Nana-Kwame1 <4211231035@live.gctu.edu.gh>
2025-10-26 23:12:34 +0000

    Updated

--- java.js
+++ java.js
@@ -1,29 +1,8 @@
-const shareTabOpen = document.querySelector('.information');
-const shareTabClosed = document.querySelector('.share-items');
+const shareBtn = document.getElementById('share-btn');
+const sharePopup = document.querySelector('.share-popup');
+const profileBadge = document.querySelector('.profile-badge');
 
-const desktopShare = document.querySelector('.desktop-share');
-const shareIcon = document.querySelector('.share');
-
-shareTabOpen.addEventListener('click', function () {
-  if (window.innerWidth < 768) {
-    shareTabOpen.style.display = 'none';
-    shareTabClosed.style.display = 'flex';
-  }
-});
-
-shareTabClosed.addEventListener('click', function () {
-  if (window.innerWidth < 768) {
-    shareTabClosed.style.display = 'none';
-    shareTabOpen.style.display = 'flex';
-  }
-});
-
-
-
-shareIcon.addEventListener('click', function (e) {
-  if (window.innerWidth > 767) {
-
-    desktopShare.classList.toggle('show');
-  }
-
+shareBtn.addEventListener('click', () => {
+  sharePopup.classList.toggle('active');
+  profileBadge.classList.toggle('hide');
 });
