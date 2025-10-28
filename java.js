9f99f1053a8ce5cbd1cba7258d815351372fec35
Elkanah <elkanah.oak@gmail.com>
2025-10-28 22:03:52 +0000

    Remove unused JavaScript file for share functionality

--- java.js
+++
@@ -1,8 +0,0 @@
-const shareBtn = document.getElementById('share-btn');
-const sharePopup = document.querySelector('.share-popup');
-const profileBadge = document.querySelector('.profile-badge');
-
-shareBtn.addEventListener('click', () => {
-  sharePopup.classList.toggle('active');
-  profileBadge.classList.toggle('hide');
-});
